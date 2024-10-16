282b006b3e475c04cc0c7b5bfba26514c73f7991
Materdelo <113280384+Materdelo@users.noreply.github.com>
2024-10-16 14:03:57 +0200

    main2

--- index.js
+++ index.js
@@ -184,10 +184,15 @@ async function processTimetableEvents(page) {
     let timetableEvents = await getTimetableEvents(page);
 
     for (const timetableEvent of timetableEvents) {
+<<<<<<< Updated upstream
       if (timetableEvent) {
         await page.goto(
           "https://synergia.librus.pl" + timetableEvent.split("'")[1],
         );
+=======
+      if (timetableEvent && timetableEvent.includes("terminarz/szczegoly/")) {
+        await page.goto("https://synergia.librus.pl" + timetableEvent.split("'")[1]);
+>>>>>>> Stashed changes
         await page.waitForSelector("body .container");
 
         const eventDetail = await fetchEventDetails(page);
